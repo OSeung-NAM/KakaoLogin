253ae7d9e767ed04232bc08c2b8e23e1ef3fad5b
NamOSeoung <developernam92@gmail.com>
2020-09-30 21:30:07 +0900

    카카오 설치여부 확인, 로그인, 로그아웃, 연결끊기 기능 구현

--- KakaoLogin/AppDelegate.swift
+++ KakaoLogin/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,8 +16,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        KakaoSDKCommon.initSDK(appKey: kakaoNativeKey)
+        
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+           if (AuthApi.isKakaoTalkLoginUrl(url)) {
+               return AuthController.handleOpenUrl(url: url)
+           }
+
+           return false
+       }
 
     // MARK: UISceneSession Lifecycle
 
@@ -31,6 +44,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("사용자 앱 종료 이벤트")
+            // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("App 실행 도중에 홈 버튼을 눌러 홈 화면으로 나간 경우1")
+            // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+            // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+        
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("App 실행 도중에 홈 버튼을 눌러 홈 화면으로 나간 경우2")
+            // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+            // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
 
+    func applicationWillEnterForeground(_ application: UIApplication) {
+           print("홈 화면에서 다시 App 아이콘을 눌러 App을 실행시킨 경우")
+           // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+       }
+       
+       func applicationDidBecomeActive(_ application: UIApplication) {
+           print("홈 화면에서 다시 App 아이콘을 눌러 App을 실행시킨 경우")
+           // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+//           KOSession.handleDidBecomeActive()
+       }
 }
 
--- KakaoLogin/ViewController.swift
+++ KakaoLogin/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import KakaoSDKAuth
+import KakaoSDKUser
 
 class ViewController: UIViewController {
 
@@ -14,6 +16,53 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func kakaoLoginBtn(_ sender: Any) {
 
+        AuthApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("loginWithKakaoAccount() success.")
+
+                //do something
+                _ = oauthToken
+            }
+        }
+    }
+    
+    //해당 디바이스 카카오 로그아웃시킴
+    @IBAction func kakaoLogout(_ sender: Any) {
+        UserApi.shared.logout {(error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("로그아웃 성공")
+            }
+        }
+    }
+    
+    //카카오톡 설치여부 확인
+    @IBAction func kakaoInstallFlagBtn(_ sender: Any) {
+        if(AuthApi.isKakaoTalkLoginAvailable()) {
+            print("카카오톡 설치됨")
+        }else {
+            print("카카오톡 미설치됨")
+        }
+    }
+    
+    @IBAction func kakaoUserDisconnectBtn(_ sender: Any) {
+        UserApi.shared.unlink {(error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("계정 Disconnect성공")
+            }
+        }
+        
+    }
+    
 }
 
